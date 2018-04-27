b77e6d2fe9277e62070724b68416e485032db527
Kieran Foley <kieran-19@hotmail.co.uk>
2018-04-27 16:08:43 +0100

    Adds animations & text to about page

--- CV/About.swift
+++ CV/About.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
+
+var profileImage:UIImageView!
+
 class About: UIViewController {
     
     @IBOutlet var linkedIn: UIButton!
     @IBOutlet var logo: UIImageView!
-    @IBOutlet var profileImage: UIImageView!
+
     
     // New instance of tabBar -> control the design / functionality
     let customTabBar = customTabBarController()
@@ -21,13 +24,21 @@ class About: UIViewController {
         super.viewDidLoad()
         // Designs the linkedIn button.
         customTabBar.redrawLinkedIn(button: linkedIn)
+
+        guard let me:UIImage = UIImage(named: "me.jpeg") else {
+            return
+        }
+        profileImage = UIImageView(image: me)
+        
         
         // Sets profile image attributes
         profileImage.layer.masksToBounds = true
         profileImage.layer.cornerRadius = 62.5
         profileImage.layer.borderColor = UIColor.white.cgColor
         profileImage.layer.borderWidth = 2
-
+        profileImage.frame = CGRect(x: -150, y: 178, width: 125, height: 125)
+        self.view.addSubview(profileImage)
+    
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -57,22 +68,8 @@ class About: UIViewController {
             CVText2.frame = CGRect(x: 140, y: 60, width: 350, height: 75)
         }
         
-    
-        
-//        let openingText = UILabel(frame: CGRect(x: 20, y: 178, width: 200, height: 125))
-
-//        let openingTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white, NSAttributedStringKey.font: UIFont(name: "AmericanTypewriter", size: 17.0)!]
-//        openingText.numberOfLines = 10
-//
-//
-//        openingText.attributedText = NSAttributedString(string: "My names's Kieran \u{1F44B} I'm a passionate 23 year old software developer currently studying at UWE Bristol.", attributes: openingTextAttributes)
-//
-//        let attributes = NSMutableAttributedString(string: "My names's Kieran \u{1F44B} I'm a passionate 23 year old software developer currently studying at UWE Bristol.", attributes: nil)
-//
-//        attributes.setAttributes([NSAttributedStringKey.foregroundColor: UIColor.white, NSAttributedStringKey.font: UIFont(name: "AmericanTypewriter", size: 17.0)!], range:1)
-        
-        
-        let openingText = UILabel(frame: CGRect(x: 19, y: 178, width: 207, height: 150))
+        // First paragraph - Adjacent to profile picture
+        let openingText = UILabel(frame: CGRect(x: 19, y: 147.5, width: 207, height: 150))
         let theString = "My names's Kieran \u{1F44B} I'm a passionate 23 year old software developer currently studying at UWE Bristol."
         let attributedString = NSMutableAttributedString(string: theString, attributes: [NSAttributedStringKey.foregroundColor: UIColor.white, NSAttributedStringKey.font: UIFont(name: "AmericanTypewriter", size: 19.0)!])
         // Bold "Kieran"
@@ -85,15 +82,40 @@ class About: UIViewController {
         openingText.attributedText = attributedString
         self.view.addSubview(openingText)
         
-
+        // First paragraph - Adjacent to profile picture
+        let skillsLabel = UILabel(frame: CGRect(x: 150, y: 300, width: 210, height: 150))
+        let skillsText = "My skills include iOS development, web technologies, C/C++ proggramming and database integration."
+        let attributedSkillsText = NSMutableAttributedString(string: skillsText, attributes: [NSAttributedStringKey.foregroundColor: UIColor.white, NSAttributedStringKey.font: UIFont(name: "AmericanTypewriter", size: 19.0)!])
+        skillsLabel.numberOfLines = 10
+        skillsLabel.attributedText = attributedSkillsText
+        self.view.addSubview(skillsLabel)
         
-            
         
         
+        UIView.animate(withDuration: 1.5, animations: {
+            profileImage.frame = CGRect(x: 234, y: 160, width: 125, height: 125)
+            var rotationAnimation = CABasicAnimation()
+            rotationAnimation = CABasicAnimation.init(keyPath: "transform.rotation.z")
+            rotationAnimation.toValue = NSNumber(value: (Double.pi))
+            rotationAnimation.duration = 0.75
+            rotationAnimation.isCumulative = true
+            rotationAnimation.repeatCount = 2.0
+            profileImage.layer.add(rotationAnimation, forKey: "rotationAnimation")
+        })
         
-        
-        
-        
+//        var logosView:UIImageView!
+//        let logoImages:[UIImage] = [UIImage(named: "swiftLogo.png")!, UIImage(named: "xcodeLogo.png")!, UIImage(named: "htmlLogo.png")!, UIImage(named: "c++Logo.png")!]
+//        logosView.frame = CGRect(x: -150, y: 300, width: 125, height: 125)
+//        
+//        UIView.animate(withDuration: 1.5, animations: {
+//            logosView.frame = CGRect(x: 75, y: 300, width: 125, height: 125)
+//            logosView = UIImageView(image: logoImages[0])
+//        }) { _ in
+//            UIView.animate(withDuration: 1.5, delay: 0.25, options: [.autoreverse, .repeat], animations: {
+//                logosView.frame = CGRect(x: 75, y: 300, width: 125, height: 125)
+//                logosView = UIImageView(image: logoImages[1])
+//            })
+//        }
     }
 
     // Declares the actions of all 5 tab bar buttons.
